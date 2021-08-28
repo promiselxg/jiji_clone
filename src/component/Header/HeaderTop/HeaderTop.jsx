1f0b856963132af2504aa35ecce60b8fb7b20633
Promiselxg <okeydeede@gmail.com>
2021-08-29 00:45:02 +0100

    login modal

--- src/component/Header/HeaderTop/HeaderTop.jsx
+++ src/component/Header/HeaderTop/HeaderTop.jsx
@@ -1,6 +1,21 @@
-import { Button, Container } from "@material-ui/core";
+import { Button, Container, Modal, Divider } from "@material-ui/core";
+import { Apple, Facebook } from "@material-ui/icons";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./HeaderTop.scss";
 const HeaderTop = () => {
+  const [open, setOpen] = useState(false);
+  const user = false;
+  const handleOpen = () => {
+    if (user === false) {
+      setOpen(true);
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       <div className="header__top">
@@ -16,9 +31,13 @@ const HeaderTop = () => {
           </div>
           <div className="header__top__right">
             <div className="nav">
-              <span>Sign In</span>
+              <Link onClick={handleOpen} to="/">
+                Sign In
+              </Link>
               <span>|</span>
-              <span>Registration</span>
+              <Link onClick={handleOpen} to="/">
+                Registration
+              </Link>
             </div>
             <div className="postAd">
               <Button>SELL</Button>
@@ -26,6 +45,34 @@ const HeaderTop = () => {
           </div>
         </Container>
       </div>
+      {/* Modal */}
+      <Modal open={open} onClick={handleClose}>
+        <div className="modal__container">
+          <div className="modal__content">
+            <div className="container">
+              <div>
+                <h3>register</h3>
+                <Button className="icon google" startIcon={<Apple />}>
+                  Register via Google
+                </Button>
+                <Button className="icon facebook" startIcon={<Facebook />}>
+                  Register via Facebook
+                </Button>
+                <span className="divider">
+                  <Divider className="divid" />
+                  <span>or</span>
+                </span>
+                <Button className="icon phone">
+                  Register via email or phone
+                </Button>
+                <span>
+                  Already have an account? <Link to="/">sign in</Link>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
     </>
   );
 };
