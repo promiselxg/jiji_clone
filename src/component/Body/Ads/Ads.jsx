4441347782b055edcee83126550c590be7bf0f88
Promiselxg <okeydeede@gmail.com>
2021-08-28 22:10:28 +0100

    ad body style

--- src/component/Body/Ads/Ads.jsx
+++ src/component/Body/Ads/Ads.jsx
@@ -1,5 +1,7 @@
 import "./Ads.scss";
 import { trending_ad } from "../../../Data/Data";
+import { IconButton } from "@material-ui/core";
+import { StarBorderOutlined } from "@material-ui/icons";
 const Ads = () => {
   return (
     <>
@@ -12,10 +14,15 @@ const Ads = () => {
             <div className="ad_item" key={i}>
               <div className="ad__image">
                 <img src={d.img} alt="ad_image" />
+                <span className="ad__special">2X TOP</span>
+                <span className="ad__picsCount">7</span>
+                <IconButton className="icon">
+                  <StarBorderOutlined />
+                </IconButton>
               </div>
               <div className="ad__info">
                 <div className="ad__title">{d.name}</div>
-                <div className="ad__price">&#8358;{d.price}</div>
+                <div className="ad__price">&#8358;&nbsp;{d.price}</div>
               </div>
             </div>
           ))}
--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -4,7 +4,58 @@ const Footer = () => {
   return (
     <>
       <div className="footer">
-        <h1>footer</h1>
+        <div className="footer__wrapper">
+          <img
+            src="https://assets.jiji.ng/static/img/footer-nigeria.svg"
+            alt="footer_image"
+          />
+        </div>
+        <div className="footer__content">
+          <div className="footer__">
+            <h3>About us</h3>
+            <span>About Jiji</span>
+            <span>We are hiring!</span>
+            <span>Terms &amp; Conditions</span>
+            <span>Privacy Policy</span>
+            <span>Billing Policy</span>
+            <span>Cookie policy</span>
+          </div>
+          <div className="footer__">
+            <h3>About us</h3>
+            <span>About Jiji</span>
+            <span>We are hiring!</span>
+            <span>Terms &amp; Conditions</span>
+            <span>Privacy Policy</span>
+            <span>Billing Policy</span>
+            <span>Cookie policy</span>
+          </div>
+          <div className="footer__">
+            <h3>our apps</h3>
+            <img
+              src="https://assets.jiji.ng/static/img/single-images/ios.svg"
+              alt="jiji_ios"
+            />
+            <img
+              src="https://assets.jiji.ng/static/img/single-images/android.svg"
+              alt="jiji_andriod"
+            />
+          </div>
+          <div className="footer__">
+            <h3>our resources</h3>
+            <span>our blog</span>
+            <span>jiji on FB</span>
+            <span>our instagram</span>
+            <span>our facebook</span>
+            <span>our twitter</span>
+          </div>
+          <div className="footer__">
+            <h3>hot links</h3>
+            <span>brand</span>
+            <span>jiji sellers</span>
+            <span>regions</span>
+            <span>jiji africa</span>
+          </div>
+        </div>
       </div>
     </>
   );
